5e1577608f7c942b8bf4ba50824ff1311d1c700a
Thais Camacho <thaiscamachoo@gmail.com>
2021-05-15 17:51:58 -0300

    is, mg e ft fixed

--- NPB3.4-MPI/exec_scripts/run_exec.sh
+++ NPB3.4-MPI/exec_scripts/run_exec.sh
@@ -9,9 +9,6 @@ mpirun -n $2 ./bt.E.x > $DATASET/$NAME-BT-E.out 2> $DATASET/$NAME-BT-E.err
 mpirun -n $2 ./cg.C.x > $DATASET/$NAME-CG-C.out 2> $DATASET/$NAME-CG-C.err
 mpirun -n $2 ./cg.D.x > $DATASET/$NAME-CG-D.out 2> $DATASET/$NAME-CG-D.err
 mpirun -n $2 ./cg.E.x > $DATASET/$NAME-CG-E.out 2> $DATASET/$NAME-CG-E.err
-mpirun -n $2 ./ep.C.x > $DATASET/$NAME-EP-C.out 2> $DATASET/$NAME-EP-C.err
-mpirun -n $2 ./ep.D.x > $DATASET/$NAME-EP-D.out 2> $DATASET/$NAME-EP-D.err
-mpirun -n $2 ./ep.E.x > $DATASET/$NAME-EP-E.out 2> $DATASET/$NAME-EP-E.err
 mpirun -n $2 ./is.C.x > $DATASET/$NAME-IS-C.out 2> $DATASET/$NAME-IS-C.err
 mpirun -n $2 ./is.D.x > $DATASET/$NAME-IS-D.out 2> $DATASET/$NAME-IS-D.err
 mpirun -n $2 ./is.E.x > $DATASET/$NAME-IS-E.out 2> $DATASET/$NAME-IS-E.err
@@ -27,3 +24,6 @@ mpirun -n $2 ./sp.E.x > $DATASET/$NAME-SP-E.out 2> $DATASET/$NAME-SP-E.err
 mpirun -n $2 ./lu.C.x > $DATASET/$NAME-LU-C.out 2> $DATASET/$NAME-LU-C.err
 mpirun -n $2 ./lu.D.x > $DATASET/$NAME-LU-D.out 2> $DATASET/$NAME-LU-D.err
 mpirun -n $2 ./lu.E.x > $DATASET/$NAME-LU-E.out 2> $DATASET/$NAME-LU-E.err
+mpirun -n $2 ./ep.C.x > $DATASET/$NAME-EP-C.out 2> $DATASET/$NAME-EP-C.err
+mpirun -n $2 ./ep.D.x > $DATASET/$NAME-EP-D.out 2> $DATASET/$NAME-EP-D.err
+mpirun -n $2 ./ep.E.x > $DATASET/$NAME-EP-E.out 2> $DATASET/$NAME-EP-E.err
